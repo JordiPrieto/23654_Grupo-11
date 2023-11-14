f3bcc77f09fd669654c321f9ccee123f9f4c39d1
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-11-14 16:19:04 -0300

    Update views.py

--- Core/views.py
+++ Core/views.py
@@ -8,3 +8,9 @@ def index(request):
 
 def productos(request):
     return render(request,'Core/productos.html')
+
+def producto(request, item):
+    context = {
+        'item' : item
+        }
+    return render(request, 'Core/producto.html', context)
