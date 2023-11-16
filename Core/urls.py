6ce1c74d8beb317bd7914c0b9457adf4e390af70
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-11-16 15:52:22 -0300

    Agrego url del form

--- Core/urls.py
+++ Core/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('productos/',views.productos,name='productos'),
-    path('productos/producto/<producto_id>', views.producto, name='producto')
+    path('productos/producto/<producto_id>', views.producto, name='producto'),
+    path('productos/registrar_producto', views.register_product, name='register_product_form'),
     # path('productos/<int:id>',views.productos_id,name='productos_id'),
     # path('contacto/',views.contacto,name='contacto')
 ]
