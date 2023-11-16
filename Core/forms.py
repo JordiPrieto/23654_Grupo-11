95b634b5174c2f18b9963a836573d7513e595a87
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-11-16 15:52:08 -0300

    Cambio campos a español

--- Core/forms.py
+++ Core/forms.py
@@ -3,30 +3,30 @@ from django.core.exceptions import ValidationError
 
 
 class ProductResgister(forms.Form):
-    prod_name = forms.CharField(max_length=45, label="Product name: ", widget=forms.TextInput(attrs={'class':'red_background'}), required=True)
+    prod_name = forms.CharField(max_length=45, label="Nombre del producto: ", widget=forms.TextInput(attrs={'class':'red_background'}), required=True)
     
-    category = forms.CharField(max_length=30, label="Category: ")
+    category = forms.CharField(max_length=30, label="Categoría: ")
 
-    stock = forms.IntegerField(label="Initial stock: ", required=False)
+    stock = forms.IntegerField(label="Stock inicial: ", required=False)
     
-    price = forms.FloatField(label="Price per unit: ", required=False)
+    price = forms.FloatField(label="Precio por unidad: ", required=False)
 
-    visible = forms.BooleanField(label="Make visible to customers? ", required=False)
+    visible = forms.BooleanField(label="Hacer visible a clientes? ", required=False)
 
-    active = forms.BooleanField(label="Active?", required=False)
+    active = forms.BooleanField(label="Activo?", required=False)
 
-    description = forms.CharField(widget=forms.Textarea, label="Description: ", required=False)
+    description = forms.CharField(widget=forms.Textarea, label="Descripción: ", required=False)
 
-    image = forms.URLField(label = "Image's URL: ", required=False)
+    image = forms.URLField(label = "URL de la imagen: ", required=False)
 
     def clean_age(self):
         if self.cleaned_data["pricce"] < 0:
-            raise ValidationError("Character can't have a negative price")
-        return self.cleaned_data["age"]
+            raise ValidationError("Un producto no puede tener un precio negativo")
+        return self.cleaned_data["price"]
 
     def clean(self):
         if self.cleaned_data['stock'] < 0:
-            raise ValidationError("Can't have a negative initial stock")
+            raise ValidationError("No se puede tener un stock inicial negativo")
         return self.cleaned_data
 
 
