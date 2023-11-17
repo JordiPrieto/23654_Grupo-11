06f46d4fa4e4d4fbd524b4af15124e814e4cd48d
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-11-17 18:10:39 -0300

    Corrijo error de tipeo y paso a localhost

--- BaxTech/settings.py
+++ BaxTech/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'BaxTech.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": {os.getenv('DB_NAME')},
+        "NAME": os.getenv('DB_NAME'),
         "USER": os.getenv('DB_USER'),
         "PASSWORD": os.getenv('DB_PASS'),
-        "HOST": "127.0.0.1",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
