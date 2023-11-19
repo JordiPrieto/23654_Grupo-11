f82310b6319e16728693cfe7603e4595e933943c
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-11-19 20:17:36 -0300

    Creo vistas para ModelForms creatdos

--- Core/views.py
+++ Core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .forms import ProductResgister
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from .forms import ProductResgister, RegisterCategory
+from .models import Category
 
 # Create your views here.
 
@@ -26,3 +29,19 @@ def register_product(request):
         'register_product': form
     }
     return render(request, "Core/register_product.html", context)
+
+class CategoryCreateView(CreateView):
+    model = Category
+    context_object_name = 'category_form'
+    template_name = 'Core/category_form.html'
+    success_url = 'category_list'
+    form_class = RegisterCategory
+    # fields = '__all__'
+
+
+class CategoryListView(ListView):
+    model = Category
+    context_object_name = "category_list"
+    template_name = 'Core/category_list.html'
+    ordering = ['category_name']
+    # systems_count = ListView.__sizeof__
