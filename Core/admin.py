1c0ef2ccc8a20f8fbd37b4dd1dc790dc76268b4c
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-11-19 21:51:50 -0300

    registro modelos en admin

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from typing import Any
+from django.db.models.fields.related import ManyToManyField
+from django.forms.models import ModelMultipleChoiceField
+from django.http.request import HttpRequest
+from Core.models import Tag, Category, Product, User, Client, Employee
+
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field == 'Employee':
+            kwargs['queryset'] = User.objects.filter(is_employee_is=False). order_by('name')
+        
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Employee, SiteAdmin)
+admin.site.register(Client)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Product)
